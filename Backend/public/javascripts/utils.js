cea67deb21566f3ef102cd9eae3a675f00fcf47f
ameer404 <a.hamza@cloudplex.io>
2020-09-15 12:15:37 +0500

    Changing folder names

--- Backend/public/javascripts/utils.js
+++
@@ -1,9 +0,0 @@
-function displayErrors(modal, errorsArray) {
-    errorsArray.map((error) => {
-        modal.find(`[data-error=${error.param}]`).text(`* ${error.msg}`);
-    });
-}
-
-function resetErrors(modal) {
-    modal.find('div[data-error]').text('');
-}
