5ceb1a4da4452cbb719992f58c1e690bde2dcea2
ameer404 <a.hamza@cloudplex.io>
2020-09-15 15:40:30 +0500

    Made backend app server public

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ var server = http.createServer(app);
 const port = 3000;
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", `${process.env.FRONTENDURL}`); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", `*`); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     next();
