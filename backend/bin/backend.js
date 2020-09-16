904dcf6a3d032aad0091b06cecf8bcfcad20a7d2
ameer404 <a.hamza@cloudplex.io>
2020-09-16 15:48:37 +0500

    Some server running changes for backend app

---
+++ backend/bin/backend.js
@@ -0,0 +1,102 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+require('dotenv').config(); // loads environment variables from a .env file into process.env
+
+
+// global.getApiServerHost = function getApiServerHost(apiServerType) {
+//     apiServerType = apiServerType === '/security' ? '/rbac'.replace(/-/g, '_') : apiServerType.replace(/-/g, '_');
+//     let apiUrl = process.env.APIURL;
+
+//     if (process.env[`${apiServerType.slice(1).toUpperCase()}URL`]) { apiUrl = process.env[`${apiServerType.slice(1).toUpperCase()}URL`]; }
+//     return apiUrl;
+// };
+
+var app = require('../app');
+var http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '5000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+global.asyncForEach = async (array, callback) => {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array)
+    }
+};
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+    const database = require('../db/mongoose/mongoose');
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    // debug('Listening on ' + bind);
+    console.log(`Listening on ${bind}`)
+}
--- backend/db/mongoose/mongoose.js
+++ backend/db/mongoose/mongoose.js
@@ -6,6 +6,16 @@ mongoose.connect(`${process.env.DBHOST}`, { useNewUrlParser: true });
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 // mongoose.set('useUnifiedTopology', true);
+const db = mongoose.connection;
+db.on('error', (e) => {
+    console.log(`**MongoError** ${e.message}`)
+    console.log(e);
+    console.log('ERROR CONNECTING TO MONGO DB :(');
+    process.exit();
+});
+db.once('open', () => {
+    console.log(`*MONGO DB RUNNING AT HOST *${process.env.DBHOST},*`);
+});
 
 module.exports = {
     mongoose
