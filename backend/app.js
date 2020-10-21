32639eb66208a1e41ee5e5ac391cdbc729a3c221
wajeeha-manzoor <70254992+wajeeha-manzoor@users.noreply.github.com>
2020-10-21 17:30:18 +0500

    Revert "route changed and save button issue resolved"

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,7 @@ app.use(express.json());
 
 app.use(upload());
 
+
 app.use(expressEjsLayout);
 // view engine setup
 
@@ -33,6 +34,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+
 
 app.use('/users', usersRouter);
 app.use('/files', fileRouter);
